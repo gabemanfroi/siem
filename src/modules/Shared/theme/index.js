6401118576479db2d05c8202485fea490afb39fa
Leonardo Casagrande <leoc.casagrande@hotmail.com>
2022-03-03 02:54:01 -0300

    :sparkles: styled login screen and added error snackbar

--- src/modules/Shared/theme/index.js
+++ src/modules/Shared/theme/index.js
@@ -1,10 +1,12 @@
 import { createTheme } from '@mui/material';
 import palette from './palette';
 import typography from './typography';
+import components from './components';
 
 const theme = createTheme({
   palette,
   typography,
+  components,
 });
 
 export default theme;
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,44 +1,71 @@
-import { useState } from 'react';
+import { Button, Card, TextField, useTheme } from '@mui/material';
+import { useFormik } from 'formik';
+import loginSchema from 'modules/Login/schemas';
+import AuthService from 'modules/Shared/api/AuthService';
+import { login } from 'modules/Shared/reducers/authReducer';
+import { setErrorMessage } from 'modules/Shared/reducers/errorReducer';
+import TokenUtil from 'modules/Shared/util/TokenUtil';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import AuthService from '../../modules/Shared/api/AuthService';
-
-import TokenUtil from '../../modules/Shared/util/TokenUtil';
-import { login } from '../../modules/Shared/reducers/authReducer';
+import { Form, StyledContainer } from './style';
 
 export default function Login() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  function onFormSubmit(event) {
-    if (event) event.preventDefault();
-    AuthService.authenticate(username, password)
-      .then((res) => {
-        const { access: token } = res;
-        TokenUtil.setToken(token);
-        dispatch(login());
-        navigate('/');
-      })
-      .catch();
-  }
+  const { spacing } = useTheme();
+
+  const { handleSubmit, getFieldProps, touched, errors } = useFormik({
+    validationSchema: loginSchema,
+    initialValues: loginSchema.getDefault(),
+    onSubmit: (values) => {
+      AuthService.authenticate(values.email, values.password)
+        .then((res) => {
+          const { access: token } = res;
+          TokenUtil.setToken(token);
+          dispatch(login());
+          navigate('/');
+        })
+        .catch(() => {
+          dispatch(setErrorMessage('AEHO'));
+        });
+    },
+  });
 
   return (
-    <form onSubmit={onFormSubmit}>
-      <input
-        type="text"
-        placeholder="username"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button type="submit">Login</button>
-    </form>
+    <StyledContainer>
+      <Card
+        sx={{
+          padding: spacing(2),
+          position: 'relative',
+          zIndex: 50,
+          minWidth: 400,
+        }}
+      >
+        <Form onSubmit={handleSubmit}>
+          <img src="navbar-logo.png" alt="navbar logo" />
+          <TextField
+            placeholder="Insira seu e-mail"
+            label="E-mail"
+            {...getFieldProps('email')}
+            fullWidth
+            error={touched.email && errors.email}
+            helperText={touched.email ? errors.email : undefined}
+          />
+          <TextField
+            type="password"
+            placeholder="Insira sua senha"
+            {...getFieldProps('password')}
+            error={touched.password && errors.password}
+            helperText={touched.password ? errors.password : undefined}
+            fullWidth
+            label="Senha"
+          />
+          <Button variant="contained" type="submit" fullWidth>
+            Login
+          </Button>
+        </Form>
+      </Card>
+    </StyledContainer>
   );
 }
