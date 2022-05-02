970d680abe52ffbcc9595e33f939b0a7b5ea5e78
Gabriel Manfroi <gabsmanfroi@gmail.com>
2022-05-02 01:01:05 -0300

    :white_check_mark: adds test for the AgentList

--- src/modules/Shared/components/Widgets/AgentList/index.tsx
+++ src/modules/Shared/components/Widgets/AgentList/index.tsx
@@ -1,35 +1,25 @@
-import { Skeleton } from '@mui/material';
-
 import { AgentType } from 'modules/Shared/types';
-import { useLoading, useDashboard } from 'modules/Shared/contexts';
+import { useDashboard } from 'modules/Shared/contexts';
 import { createAgentListMock } from 'modules/Shared/helpers/factories/mocks/AgentMock';
+import { LoadingHandler } from 'modules/Shared/components';
 import { Container } from './style';
 import Agent from './Agent';
 
 export default function AgentList() {
-  const { isLoading } = useLoading();
   const { groupedByAgent } = useDashboard();
 
   return (
     <Container>
-      {isLoading && (
-        <Skeleton
-          data-testid="AgentListSkeleton"
-          variant="rectangular"
-          sx={{ flex: 1, borderRadius: '5px' }}
-          animation="wave"
-        />
-      )}
-      {!isLoading &&
-        process.env.REACT_APP_ENVIRONMENT === 'production' &&
-        groupedByAgent.map((agent: AgentType) => (
-          <Agent key={agent.generalData.id} agent={agent} />
-        ))}
-      {!isLoading &&
-        process.env.REACT_APP_ENVIRONMENT === 'development' &&
-        createAgentListMock(7).map((agent: AgentType) => (
-          <Agent key={agent.generalData.id} agent={agent} />
-        ))}
+      <LoadingHandler>
+        {process.env.REACT_APP_ENVIRONMENT === 'production' &&
+          groupedByAgent.map((agent: AgentType) => (
+            <Agent key={agent.generalData.id} agent={agent} />
+          ))}
+        {process.env.REACT_APP_ENVIRONMENT === 'development' &&
+          createAgentListMock(7).map((agent: AgentType) => (
+            <Agent key={agent.generalData.id} agent={agent} />
+          ))}
+      </LoadingHandler>
     </Container>
   );
 }
