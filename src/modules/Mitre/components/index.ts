13e5c275e3a31e8d07c8e46d82eb6d8b85e8cf69
Gabriel Manfroi <gabsmanfroi@gmail.com>
2022-05-05 23:28:20 -0300

    :building_construction: starts refactoring for new widgets [sc-86]

--- src/modules/Mitre/components/index.ts
+++ src/modules/Mitre/components/index.ts
@@ -1,2 +1,4 @@
 export { default as AttacksByTechnique } from './AttacksByTechnique';
 export { default as TechniquesByAgent } from './TechniquesByAgent';
+export { default as TopTactics } from './TopTactics';
+export { default as TopTacticsByAgent } from './TopTacticsByAgent';
