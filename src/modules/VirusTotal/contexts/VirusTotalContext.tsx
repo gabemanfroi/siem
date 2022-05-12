c0ff41d6ac9bd1595fbdb8da2d748d57c7f806c9
Gabriel Manfroi <gabsmanfroi@gmail.com>
2022-05-12 14:22:33 -0300

    :building_construction: adds Virus Total Module to the Application [sc-86]

---
+++ src/modules/VirusTotal/contexts/VirusTotalContext.tsx
@@ -0,0 +1,66 @@
+import React, {
+  createContext,
+  Dispatch,
+  SetStateAction,
+  useContext,
+  useMemo,
+  useState,
+} from 'react';
+import {
+  IAlertsEvolutionByAgents,
+  ILastScannedFiles,
+  IUniqueMaliciousFilesPerAgent,
+} from 'modules/VirusTotal/interfaces';
+
+interface IVirusTotalContext {
+  alertsEvolutionByAgents: IAlertsEvolutionByAgents | undefined;
+  lastScannedFiles: ILastScannedFiles | undefined;
+  uniqueMaliciousFilesPerAgent: IUniqueMaliciousFilesPerAgent | undefined;
+  widgetsHandlersMap: { [key: string]: Dispatch<SetStateAction<any>> };
+}
+
+const virusTotalContextDefaultValues: IVirusTotalContext = {
+  alertsEvolutionByAgents: undefined,
+  lastScannedFiles: undefined,
+  uniqueMaliciousFilesPerAgent: undefined,
+  widgetsHandlersMap: {},
+};
+
+const VirusTotalContext = createContext<IVirusTotalContext>(
+  virusTotalContextDefaultValues
+);
+
+export const VirusTotalProvider: React.FC = ({ children }) => {
+  const [alertsEvolutionByAgents, setAlertsEvolutionByAgents] = useState<
+    IAlertsEvolutionByAgents | undefined
+  >();
+  const [lastScannedFiles, setLastScannedFiles] = useState<
+    ILastScannedFiles | undefined
+  >();
+  const [uniqueMaliciousFilesPerAgent, setUniqueMaliciousFilesPerAgent] =
+    useState<IUniqueMaliciousFilesPerAgent | undefined>();
+
+  const widgetsHandlersMap = {
+    alertsEvolutionByAgents: setAlertsEvolutionByAgents,
+    lastScannedFiles: setLastScannedFiles,
+    uniqueMaliciousFilesPerAgent: setUniqueMaliciousFilesPerAgent,
+  };
+
+  const value = useMemo(
+    () => ({
+      alertsEvolutionByAgents,
+      lastScannedFiles,
+      uniqueMaliciousFilesPerAgent,
+      widgetsHandlersMap,
+    }),
+    [alertsEvolutionByAgents, lastScannedFiles, uniqueMaliciousFilesPerAgent]
+  );
+
+  return (
+    <VirusTotalContext.Provider value={value}>
+      {children}
+    </VirusTotalContext.Provider>
+  );
+};
+
+export const useVirusTotal = () => useContext(VirusTotalContext);
