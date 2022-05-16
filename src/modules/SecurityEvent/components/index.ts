12743e503961aa5892a94418f735220f34d74c58
Gabriel Manfroi <gabsmanfroi@gmail.com>
2022-05-16 11:26:11 -0300

    :building_construction: adds structural files to the new module  [sc-86]

---
+++ src/modules/SecurityEvent/components/index.ts
@@ -0,0 +1,4 @@
+export { default as Top5Agents } from './Top5Agents';
+export { default as AlertLevelEvolution } from './AlertLevelEvolution';
+export { default as AlertsEvolutionTop5Agents } from './AlertsEvolutionTop5Agents';
+export { default as TopMitre } from './TopMitre';
---
+++ src/modules/SecurityEvent/interfaces/index.ts
@@ -0,0 +1,4 @@
+export { default as IAlertLevelEvolution } from './IAlertLevelEvolution';
+export { default as IAlertsEvolutionTop5Agents } from './IAlertsEvolutionTop5Agents';
+export { default as ITop5Agents } from './ITop5Agents';
+export { default as ITopMitre } from './ITopMitre';
