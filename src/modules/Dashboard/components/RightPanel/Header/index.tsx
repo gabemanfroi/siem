3f41f379659736564e64dbc4d645581c50f4693d
Gabriel Manfroi <gabsmanfroi@gmail.com>
2022-05-26 16:42:18 -0300

    :coffin: refactors header to use new selector [sc-22]

--- src/modules/Dashboard/components/RightPanel/Header/index.tsx
+++ src/modules/Dashboard/components/RightPanel/Header/index.tsx
@@ -1,30 +1,14 @@
-import { AppBar, Autocomplete, Box, TextField, Toolbar } from '@mui/material';
-import { useWidgets } from 'modules/Shared/contexts';
+import { AppBar, Box, Toolbar } from '@mui/material';
+import WidgetsSelector from './WidgetsSelector';
 
-const Header = () => {
-  const { widgetsList } = useWidgets();
-
-  return (
-    <Box>
-      <AppBar position="static">
-        <Toolbar sx={{ justifyContent: 'flex-end', padding: '16px' }}>
-          <Autocomplete
-            sx={{ minWidth: '300px' }}
-            multiple
-            filterSelectedOptions
-            renderInput={(params) => (
-              <TextField
-                {...params}
-                label="Adicionar Widget"
-                placeholder="Adicionar Widget"
-              />
-            )}
-            options={widgetsList}
-          />
-        </Toolbar>
-      </AppBar>
-    </Box>
-  );
-};
+const Header = () => (
+  <Box>
+    <AppBar position="static">
+      <Toolbar sx={{ justifyContent: 'flex-end', padding: '16px' }}>
+        <WidgetsSelector />
+      </Toolbar>
+    </AppBar>
+  </Box>
+);
 
 export default Header;
