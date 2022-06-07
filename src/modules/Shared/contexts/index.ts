ad680eeccb7e81be30a01a6519d781d2f02c67a3
Gabriel Manfroi <gabsmanfroi@gmail.com>
2022-06-07 12:38:37 -0300

    :building_construction: structural changes [sc-107]

--- src/modules/Shared/contexts/index.ts
+++ src/modules/Shared/contexts/index.ts
@@ -1,4 +1,3 @@
 export { WidgetsProvider, useWidgets } from './WidgetsContext';
 export { AgentProvider, useAgent } from './AgentContext';
-export { DashboardProvider, useDashboard } from './DashboardContext';
 export { LoadingProvider, useLoading } from './LoadingContext';
