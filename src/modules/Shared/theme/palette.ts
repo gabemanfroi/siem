cdb9e1e19e350879e4e7518ef1438cafcc77e144
Gabriel Manfroi <gabsmanfroi@gmail.com>
2022-06-07 12:53:10 -0300

    :lipstick: updates palette schema [sc-107]

--- src/modules/Shared/theme/palette.ts
+++ src/modules/Shared/theme/palette.ts
@@ -2,20 +2,36 @@ import { PaletteOptions } from '@mui/material';
 import {
   dark200,
   dark500,
-  primary600,
-  primary700,
   primaryBlue,
   primaryRed,
   primaryYellow,
   gray300,
   white,
+  primary100,
+  primary300,
+  primary200,
+  primary400,
+  primary500,
+  primary600,
+  primary700,
+  primary800,
 } from '../helpers/styles/Colors';
 
 const palette: PaletteOptions = {
   mode: 'dark',
+  grey: {},
   primary: {
     main: primary700,
     contrastText: white,
+    100: primary100,
+    200: primary200,
+    300: primary300,
+    400: primary400,
+    500: primary500,
+    600: primary600,
+    700: primary700,
+    800: primary800,
+    light: primary100,
   },
   secondary: {
     main: primaryBlue,
