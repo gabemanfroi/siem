8889f542c00e1adc2afac66646563f21da93d616
Gabriel Manfroi <manfroigab@gmail.com>
2022-08-31 18:14:10 -0300

    :construction_worker: changes to ci/cd

--- jenkins.sh
+++ jenkins.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 su - jenkins <<!
 jenkins
-docker build -tag front --no-cache  .
+docker build - front --no-cache  .
 docker tag front gabemanfroi/siem_frontend:production
 docker push gabemanfroi/siem_frontend:production
 microk8s kubectl delete -f /home/gabriel/yggdrasil/kubectl/production/front/deployment.yml
 microk8s kubectl apply -f /home/gabriel/yggdrasil/kubectl/production/front/deployment.yml
-
+!
