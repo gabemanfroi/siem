33a4f1bb8e21c880da99e95de44fb6d158663718
Gabriel Manfroi <manfroigab@gmail.com>
2022-08-31 17:48:41 -0300

    :construction_worker: changes to ci/cd

--- jenkins.sh
+++ jenkins.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 su - jenkins <<!
 jenkins
-docker build -t front --build-arg REACT_APP_ENVIRONMENT='production' --build-arg REACT_APP_HTTP_API_URL='https://apigw.seclab.inf.br' --build-arg REACT_APP_TOKEN_KEY_NAME='@seclab_token' --build-arg REACT_APP_WIDGETS_CONFIG_NAME='@seclab_widgets_config ' .
+docker build --tag front .
 docker tag front gabemanfroi/siem_frontend:production
 docker push gabemanfroi/siem_frontend:production
 microk8s kubectl delete -f /home/gabriel/yggdrasil/kubectl/production/front/deployment.yml
