840a811a90a89d4e6ec9d0364e0766bf436c1d77
Gabriel Manfroi <gabriel.manfroi@edreamsodigeo.com>
2023-07-01 13:13:40 +0100

    🔨rearranges endpoints

--- src/modules/Shared/constants/routes/Bragi.ts
+++ src/modules/Shared/constants/routes/Bragi.ts
@@ -2,17 +2,17 @@ const initialPath =
   process.env.REACT_APP_ENVIRONMENT === 'development' ? '' : '/bragi';
 const BRAGI = {
   BASE_ENDPOINT: initialPath,
-  DASHBOARD: `${initialPath}/api/dashboard`,
+  DASHBOARD: `${initialPath}/dashboard`,
   DASHBOARD_WIDGETS: `${initialPath}/widgets/dashboard`,
-  INTEGRITY_MONITORING: `${initialPath}/api/integrity_monitoring`,
-  MITRE: `${initialPath}/api/mitre`,
-  SECURITY_EVENT: `${initialPath}/api/security_event`,
+  INTEGRITY_MONITORING: `${initialPath}/integrity_monitoring`,
+  MITRE: `${initialPath}/mitre`,
+  SECURITY_EVENT: `${initialPath}/security_event`,
   SECURITY_EVENT_WIDGETS: `${initialPath}/widgets/security-event`,
-  VIRUS_TOTAL: `${initialPath}/api/virus_total`,
-  VULNERABILITY: '/bragi/api/vulnerability',
+  VIRUS_TOTAL: `${initialPath}/virus_total`,
+  VULNERABILITY: `${initialPath}/vulnerability`,
   AGENT: `${initialPath}/agent`,
   AGENT_WIDGETS: `${initialPath}/widgets/agent`,
   SCA: `${initialPath}/sca`,
-  ANALYSIS: `${initialPath}/api/analysis`,
+  ANALYSIS: `${initialPath}/analysis`,
 };
 export default BRAGI;
